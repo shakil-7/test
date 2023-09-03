731c910fd5f11a3dcdee565621e3340ef67231c3
mahadi-hasan <mahadi.hasan@bKash.com>
2023-09-03 13:15:36 +0600

    testing phase-1

---
+++ src/test/java/com/example/test/controller/TestControllerTest.java
@@ -0,0 +1,19 @@
+package com.example.test.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(classes = TestController.class)
+class TestControllerTest {
+
+
+    @Test
+    public void test1() {
+
+    }
+
+    @Test
+    public void test2() {
+//        Assertions.fail();
+    }
+}
