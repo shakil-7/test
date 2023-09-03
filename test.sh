a60c0f6e239c0cdf7a9d41e5719b169093cf23c0
mahadi-hasan <mahadi.hasan@bKash.com>
2023-09-03 14:55:17 +0600

    dafdas

--- test.sh
+++ test.sh
@@ -6,8 +6,11 @@ echo "Running integration test before commit..."
 mvn clean test
 ' > pre-commit
 
+chmod +x pre-commit
+
 echo '#!/bin/bash
 echo "Running integration test before push..."
 mvn clean test
 ' > pre-push
 
+chmod +x pre-push
